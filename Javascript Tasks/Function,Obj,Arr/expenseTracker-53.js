a0b0392eeba0ad69006e829565a69f31a4eab3d2
Anbu2011 <anbuseenuvasan2002@gmail.com>
2024-08-20 16:58:41 +0530

    Added Expense Tracker - 53

---
+++ Javascript Tasks/Function,Obj,Arr/expenseTracker-53.js
@@ -0,0 +1,62 @@
+class Expense{
+    constructor(id,name,amount,date,category,paymentMethod){
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+        this.date = date;
+        this.category = category;
+        this.paymentMethod = paymentMethod;
+    }
+}
+
+function displayExpenses(expenses){
+    expenses.forEach(ele => {
+        console.log(`${ele.name} - ${ele.amount} (${ele.date})`)
+    });
+}
+
+function addExpense(id, name, amount, date, category, paymentMethod){
+    const newExpense = new Expense(id, name, amount, date, category, paymentMethod)
+    expenses.push(newExpense)
+    console.log("\nNew Expense Added",expenses)
+
+}
+
+function updateExpense(id,amount){
+    const update = expenses.find(ele => ele.id === id)
+    update.amount = amount
+    console.log("\n Updated Amount in expenses",expenses)
+}
+
+function updateExpenseWithMap(id,amount){
+    const updateWithMap = expenses.map(ele =>{
+        if(ele.id === id){
+            ele.amount = amount
+        }
+        return ele
+    })
+    console.log("\nNew Array Updated with Map", updateWithMap)
+}
+
+function removeExpense(id){
+    const remove_expense = expenses.filter(ele => ele.id !== id)
+    expenses.length = 0
+    expenses.push(...remove_expense)
+    
+    console.log("\nExpenses Array with Removed Expense",expenses)
+
+}
+
+const expenses = [
+    {id: 301, name: "Groceries", amount: 150.75, date: "2024-08-01", category: "Food", paymentMethod: "Credit Card"},
+    {id: 302, name: "Electricity Bill", amount: 80.00, date: "2024-08-05", category: "Utilities", paymentMethod: "Bank Transfer"},
+    {id: 303, name: "Internet Subscription", amount: 50.00, date: "2024-08-10", category: "Utilities", paymentMethod: "Credit Card"},
+    {id: 304, name: "Gym Membership", amount: 45.00, date: "2024-08-12", category: "Health & Fitness", paymentMethod: "Debit Card"},
+    {id: 305, name: "Car Maintenance", amount: 200.00, date: "2024-08-15", category: "Transportation", paymentMethod: "Cash"},
+]
+
+displayExpenses(expenses)
+addExpense(306,"Dinner at Restaurant", 75.50, "2024-08-18", "Entertainment", "Credit Card")
+updateExpense(304,500)
+updateExpenseWithMap(305,1500)
+removeExpense(302)
