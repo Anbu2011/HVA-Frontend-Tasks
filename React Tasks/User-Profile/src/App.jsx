b015ac75cf92aa0296fcc303075df815991a8537
Anbu2011 <anbuseenuvasan2002@gmail.com>
2024-08-26 10:18:28 +0530

    Added User profile's hard coded data - 64(a)

---
+++ React Tasks/User-Profile/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import UserProfile from './UserProfile'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>User Profile</h1>
+      </div>
+      <UserProfile />
+    </>
+  )
+}
+
+export default App
---
+++ React Tasks/User-Profile/src/UserProfile.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './UserProfile.css'
+const UserProfile = () => {
+    
+    return (
+    <>
+        
+        <div className='profile'>
+            <p>Name : Ajay</p>
+            <p>Age : 21</p>
+            <p>Bio : Software Engineer</p> 
+        </div>
+        
+    </>
+  )
+}
+
+export default UserProfile
