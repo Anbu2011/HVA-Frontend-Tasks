a710830723c07452b33867c711d9c758742c18e1
Anbu2011 <anbuseenuvasan2002@gmail.com>
2024-08-29 02:05:05 +0530

    Added Weather Data - 65(e)

--- React Tasks/Weather-Display/src/App.jsx
+++ React Tasks/Weather-Display/src/App.jsx
@@ -3,26 +3,10 @@ import WeatherDisplay from './WeatherDisplay'
 
 function App() {
   
-  const weatherArray = [
-    {temperature: '72°C', condition: 'Sunny', location: 'Los Angeles, CA'},
-    {temperature: '65°C', condition: 'Sunny', location: 'San Francisco, CA'},
-    {temperature: '50°C', condition: 'Rainy', location: 'Seattle, WA'},
-    {temperature: '85°C', condition: 'Sunny', location: 'Phoenix, AZ'},
-    {temperature: '30°C', condition: 'Rainy', location: 'Denver, CO'}
-
-  ]
-
-  
-
   return (
     <>
-      <h1>Weather Display</h1>
-      <div>
-        {weatherArray.map((elements) => (
-          <WeatherDisplay weather={elements} />
-        ))}
-      </div>
-      
+      <h1>Weather Forecast</h1>
+      <WeatherDisplay />
       
     </>
   )
--- React Tasks/Weather-Display/src/WeatherDisplay.jsx
+++ React Tasks/Weather-Display/src/WeatherDisplay.jsx
@@ -1,29 +1,57 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './WeatherDisplay.css'
+import axios from 'axios'
 
+const WeatherDisplay = () => {
 
-const WeatherDisplay = (props) => {
-  const {weather} = props
-  const {temperature, condition, location} = weather
+  const [location, setLocation] = useState("")
+  const [fullLocation, setFullLocation] = useState("")
+  const [displayWeather, setDisplayWeather] = useState(null)
 
-  const [updateWeather, setUpdateWeather] = useState(condition)
-  
-  const update = () =>{
-      setUpdateWeather(prevCondition => prevCondition === 'Sunny' ? 'Rainy' : 'Sunny')
+  const handleLocationChange = (event) =>{
+    setLocation(event.target.value)
   }
 
-  return (
-    <>
-        <div className='weather'>
-            <p><strong>Temperature : </strong>{temperature}</p>
-            <p><strong>Location : </strong>{location}</p>
+  const handleLocationSubmit = () =>{
+    setFullLocation(location)
+    setLocation("")
+  }
 
-            <p><strong>Condition : </strong>{updateWeather}</p>
-            
-            <button onClick={update}>Update</button>
-        </div>
-        
-        
+  const getWeatherData = async () => {
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${fullLocation}&appid=7ec993d4f8aec15578e1a6bf1481addc&units=metric`);
+      setDisplayWeather(response.data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  useEffect(() => {
+    if(fullLocation){
+      getWeatherData();
+    }
+  }, [fullLocation]); 
+  
+  return (
+    <>  
+      
+      <div className='user-input'>
+        <label htmlFor="location" >Location : </label>
+        <input type="text" value={location} onChange={handleLocationChange} placeholder='Enter the Town/City'/>
+        <button className="submit-button" onClick={handleLocationSubmit}>Submit</button>
+      </div>
+
+      <div className='weather-parent'>
+        {displayWeather && (
+          <div className='weather'>
+            <h2>{displayWeather.name}</h2>
+            <p><strong>Temperature : </strong>{displayWeather.main.temp}°C</p>
+            <p><strong>Condition : </strong>{displayWeather.weather[0].main}</p>
+            <p><strong>Country : </strong>{displayWeather.sys.country}</p>
+          </div>
+        )}
+      </div>
+      
 
     </>
   )
